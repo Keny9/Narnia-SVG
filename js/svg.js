f6fbec31c0da9ef6b19f9f229bf84b3daf0d957b
Karl <karlito1998@hotmail.com>
2019-12-16 11:46:44 -0500

    Étonnement personnage + son magique

--- js/svg.js
+++ js/svg.js
@@ -26,6 +26,16 @@ function initializeAudio(){
   audioOpenLocker = document.createElement("audio");
   document.body.appendChild(audio);
   audioOpenLocker.src = "./sound/open_locker.ogg";
+
+  //Son pour la transition de scene
+  audioTransit = document.createElement("audio");
+  document.body.appendChild(audioTransit);
+  audioTransit.src = "./sound/magic-effect.mp3";
+
+  //Son pour le personngae (Male-scared-gasp)
+  audioScared = document.createElement("audio");
+  document.body.appendChild(audioScared);
+  audioScared.src = "./sound/male-scared-gasp.mp3";
 }
 
 //Commencer la story
@@ -62,10 +72,23 @@ function openLocker(){
   $("#line3").remove();
   audioOpenLocker.play();
   audioSchool.pause();
-  setTimeout(changeScene, 5000);
+  setTimeout(scaredGuy, 3000);
+}
+
+//Étonnement du personnage
+function scaredGuy(){
+  audioScared.play();
+  setTimeout(changeScene, 3000);
 }
 
 //Changement de scene
 function changeScene(){
+  audioTransit.play();
   $("#schoolScene").addClass("endSchoolScene");
+  setTimeout(startNarniaScene, 5000);
+}
+
+//Nouvelle Scene (Narnia)
+function startNarniaScene(){
+  audioTransit.pause();
 }
